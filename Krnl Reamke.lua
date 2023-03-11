f92ada26a0c980c4da2db9e60b3d54c31d5a4e90
Qwiz <125666833+qqwizzixxxx@users.noreply.github.com>
2023-03-11 20:24:23 +0300

    Create Krnl Reamke.lua

---
+++ Krnl Reamke.lua
@@ -0,0 +1,14 @@
+En
+Link To RemakeModel: https://web.roblox.com/library/12753203438/Executor
+Step 1:Get It Model, Go to link Press Green Button "Get",And press "Get Now"
+Step 2:Press Try In Studio
+Step 3:You See Toolbox Window Press "Inventory" And Press "Executor"
+Step 4:In Explorer You See Model "Put In StarterGui!" Put It Model In StarterGui And Have Fun! :)
+
+
+Ru
+Ссылка На РемейкМодель: https://web.roblox.com/library/12753203438/Executor
+Шаг 1:Получить Модель, Зайди По Ссылке Нажми Зелёную Кнопку "Получить Или Get", И Потом Нажми "Получить Сейчас Или Get Now"
+Шаг 2:Потом Нажми "Попробовать В Студии Или Try in Studio"
+Шаг 3:Ты Видишь Окно Тулбокса Нажми "Инвентарь Или Inventory" И Нажми "Ексекутер Или Executer"
+Шаг 4:В  Експлорере/Explorer Ты Видишь Модель "Перенеси В СтартерГуи! Или Put In StaterGui!" Перенеси Эту Модель В СтартерГуи/StarteGui И Веселись! :)
