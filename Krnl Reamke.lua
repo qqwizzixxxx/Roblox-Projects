b28eae824a6e52f09e50dba670b7061cce61e939
Qwiz <125666833+qqwizzixxxx@users.noreply.github.com>
2023-03-11 20:31:09 +0300

    Update Krnl Reamke.lua

--- Krnl Reamke.lua
+++ Krnl Reamke.lua
@@ -1,4 +1,5 @@
 En
+Test Game: https://web.roblox.com/games/12753567188/Executor-Test
 Link To RemakeModel: https://web.roblox.com/library/12753203438/Executor
 Step 1:Get It Model, Go to link Press Green Button "Get",And press "Get Now"
 Step 2:Press Try In Studio
@@ -7,6 +8,7 @@ Step 4:In Explorer You See Model "Put In StarterGui!" Put It Model In StarterGui
 
 
 Ru
+Тест Игра: https://web.roblox.com/games/12753567188/Executor-Test
 Ссылка На РемейкМодель: https://web.roblox.com/library/12753203438/Executor
 Шаг 1:Получить Модель, Зайди По Ссылке Нажми Зелёную Кнопку "Получить Или Get", И Потом Нажми "Получить Сейчас Или Get Now"
 Шаг 2:Потом Нажми "Попробовать В Студии Или Try in Studio"
