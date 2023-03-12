57997a09fa854a808e9c1926f5bcfa6bb6e6208e
Qwiz <125666833+qqwizzixxxx@users.noreply.github.com>
2023-03-12 16:59:19 +0300

    Update Krnl Reamke.lua

--- Krnl Reamke.lua
+++ Krnl Reamke.lua
@@ -1,18 +1,16 @@
 En
 Test Game: https://web.roblox.com/games/12753567188/Executor-Test
-Link To RemakeModel: https://web.roblox.com/library/12763116962/New-Model
+Link To RemakeModel: https://web.roblox.com/library/12764977234/ModelXd
 Step 1:Get It Model, Go to link Press Green Button "Get",And press "Get Now"
 Step 2:Press Try In Studio
-Step 3:You See Toolbox Window Press "Inventory" And Press "New Model"
+Step 3:You See Toolbox Window Press "Inventory" And Press "ModelXd"
 Step 4:In Explorer You See Model "Put In StarterGui(Krnl)!" Put It Model In StarterGui And Have Fun! :)
-Model Deleted
 
 
 Ru
 Тест Игра: https://web.roblox.com/games/12753567188/Executor-Test
-Ссылка На РемейкМодель: https://web.roblox.com/library/12763116962/New-Model
+Ссылка На РемейкМодель: https://web.roblox.com/library/12764977234/ModelXd
 Шаг 1:Получить Модель, Зайди По Ссылке Нажми Зелёную Кнопку "Получить Или Get", И Потом Нажми "Получить Сейчас Или Get Now"
 Шаг 2:Потом Нажми "Попробовать В Студии Или Try in Studio"
-Шаг 3:Ты Видишь Окно Тулбокса Нажми "Инвентарь Или Inventory" И Нажми "Новая Модель Или New Miodel"
+Шаг 3:Ты Видишь Окно Тулбокса Нажми "Инвентарь Или Inventory" И Нажми "МодельХз Или ModelXd"
 Шаг 4:В  Експлорере/Explorer Ты Видишь Модель "Перенеси В СтартерГуи(Крнл)! Или Put In StaterGui(Krnl)!" Перенеси Эту Модель В СтартерГуи/StarterGui И Веселись! :)
-Модель удалили
