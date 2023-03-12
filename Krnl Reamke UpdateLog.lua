fab75d4e3727577e78f77bc99a455e63e9dc5566
Qwiz <125666833+qqwizzixxxx@users.noreply.github.com>
2023-03-12 13:50:58 +0300

    Create Krnl Reamke UpdateLog.lua

---
+++ Krnl Reamke UpdateLog.lua
@@ -0,0 +1,10 @@
+--[[11.03.23 Update Log:
+1.Fixed Bugs With EXECUTE,Inject
+2.New Buttons:CLOSE CONSOLE,ToggleUi,CloseUi
+3.Created Update Log File!
+4.Removed OPEN CONSOLE Button
+5.New Grafic Interface
+ 12.03.23 Update Log:
+1.Fixed Bugs With CloseUI,Execute
+2.New Buttons:Credits,CloseCredits
+]]
