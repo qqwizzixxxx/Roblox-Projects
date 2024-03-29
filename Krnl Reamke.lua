eb83653e241e7597204b03a73e99751872ee1eb3
Qwiz <125666833+qqwizzixxxx@users.noreply.github.com>
2024-03-29 20:23:52 +0300

    Update Krnl Reamke.lua

--- Krnl Reamke.lua
+++ Krnl Reamke.lua
@@ -1,6 +1,6 @@
 En
 Test Game: https://web.roblox.com/games/12753567188/Executor-Test
-Link To RemakeModel: https://web.roblox.com/library/12881119594/ModelXd
+Link To RemakeModel: https://web.roblox.com/library/12784946442/ModelXd
 Step 1:Get Model, Go to link Press Green Button "Get",And press "Get Now"
 Step 2:Press Try In Studio
 Step 3:You See Toolbox Window Press "Inventory" And Press "ModelXd"
@@ -9,7 +9,7 @@ Step 4:In Explorer You See Model "Put In StarterGui!" Put It Model In StarterGui
 
 Ru
 Тест Игра: https://web.roblox.com/games/12753567188/Executor-Test
-Ссылка На РемейкМодель: https://web.roblox.com/library/12881119594/ModelXd
+Ссылка На РемейкМодель: https://web.roblox.com/library/12784946442/ModelXd
 Шаг 1:Получить Модель, Зайди По Ссылке Нажми Зелёную Кнопку "Получить Или Get", И Потом Нажми "Получить Сейчас Или Get Now"
 Шаг 2:Потом Нажми "Попробовать В Студии Или Try in Studio"
 Шаг 3:Ты Видишь Окно Тулбокса Нажми "Инвентарь Или Inventory" И Нажми "МодельХз Или ModelXd"
